8bb9dd935209c46ce5f10c350f6e2edc130f2bcf
Julian <julian.roller@web.de>
2019-05-11 18:13:02 +0200

    Buttons schöner angeordnet

--- gui/guimain.py
+++ gui/guimain.py
@@ -1,5 +1,7 @@
 from guizero import App, Text, Picture, PushButton
 
+from gui.guizero import Box
+
 
 class Gui(object):
     # text, picture = {}
@@ -9,12 +11,16 @@ class Gui(object):
     # The class "constructor" - It's actually an initializer
     def __init__(self, text, picture, buttons):
         self.app = App(title="guessthepartei")
-        self.text = Text(self.app, text=text)
+
         self.picture = Picture(self.app, image=picture)
+        self.text = Text(self.app, text=text)
         print(buttons)
+        self.buttonbox = Box(self.app, layout="grid")
+        i = 0;
         for x in buttons:
             print(x)
-            self.buttons.append(PushButton(self.app, command=x['command'], text=x['text']))
+            self.buttons.append(PushButton(self.buttonbox, command=x['command'], grid=[i, 0],  text=x['text']))
+            i += 1
         self.app.display()
 
     def set_picture(self, picture):
