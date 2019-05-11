8d5ec33ce4923ffe32944c55defd3f4d132e402d
Anna <asinitsyna@inovex.de>
2019-05-11 18:11:53 +0200

    added immage search but it doesnt work yet

---
+++ imageSearch/search.py
@@ -0,0 +1,33 @@
+import os
+
+from google_images_search import GoogleImagesSearch
+
+gis = GoogleImagesSearch('AIzaSyCcrhOwuC5TK_O18yjeSTCuXEHVxk-9LTQ', '005383774336154718355:xu2fzioh270')
+
+partei_list = ['CDU', 'Alternative fuer Deutschland', 'Tierschutzpartei']
+
+# define search params:
+_search_params = {
+    'q': 'wahlplakat 2019',
+    'num': 2,
+    'safe': 'off',
+    'fileType': 'jpg|gif|png',
+    'imgSize': 'medium',
+    'searchType': 'image'
+    # 'imgDominantColor': 'black|blue|brown|gray|green|pink|purple|teal|white|yellow'
+}
+
+# search first, then download and resize afterwards
+for partei in partei_list:
+
+    _search_params.update({'q': partei + ' wahlplakat 2019'})
+
+    picDir = 'pics/' + partei.replace(" ", "")
+
+    if not os.path.exists(picDir):
+        os.mkdir(picDir)
+
+    gis.search(_search_params)
+    for image in gis.results():
+        image.download(picDir)
+        image.resize(500, 500)
