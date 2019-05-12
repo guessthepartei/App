d84ece466e9811c028f07a8a7e8cadf66e87e14e
Anna <asinitsyna@inovex.de>
2019-05-12 13:34:56 +0200

    add word correction

--- magik/main.py
+++ magik/main.py
@@ -4,6 +4,7 @@ import random
 # import parse.py
 import subprocess
 from subprocess import PIPE, run
+import re
 
 def out(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
@@ -18,12 +19,16 @@ def main():
    party = random.choice(partys)
 
 
-   print(party)
+   #print(party)
    plakats = os.listdir('dataset/{}'.format(party))
 #print(plakats)
 
    plakat = random.choice(plakats)
-#print(plakat)
+   #print(plakat)
 
-   return "dataset/{}/{}".format(party, plakat), parsing.parsing("{}/{}".format(party, plakat))
+
+   slogan = re.sub(party, '____', parsing.parsing("{}/{}".format(party, plakat)), re.IGNORECASE)
+   #print(slogan)
+
+   return "dataset/{}/{}".format(party, plakat), slogan
 #os.system("python3 parsing.py {}/{}".format(party, plakat))
